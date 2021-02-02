79d5283422ab4e4b0bbbb6ffff6f16eaaeb8aced
hanxiangyang <hans.young@outlook.com>
2021-02-02 23:05:54 +0800

    重新理解KMP算法

--- leetcode/src/main/java/lc/book/arrayAndString/chap04/FindSubstring.java
+++ leetcode/src/main/java/lc/book/arrayAndString/chap04/FindSubstring.java
@@ -1,7 +1,7 @@
 package lc.book.arrayAndString.chap04;
 
 import java.time.LocalDate;
-import java.util.Arrays;
+import java.util.Objects;
 
 /**
  *  题目描述：
@@ -14,11 +14,15 @@ public class FindSubstring {
     public static void main(String[] args) {
         LocalDate.now().minusMonths(1).withDayOfMonth(1);
         FindSubstring instance = new FindSubstring();
-        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("abcdabcd".toCharArray())));
-        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("abcdabcda".toCharArray())));
-        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("aba".toCharArray())));
-        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("abab".toCharArray())));
-        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("ababa".toCharArray())));
+//        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("abcdabcd".toCharArray())));
+//        System.out.println(Arrays.toString(instance.getNext("abcdabcd".toCharArray())));
+//        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("abcdabcda".toCharArray())));
+//        System.out.println(Arrays.toString(instance.getNext("abcdabcda".toCharArray())));
+//        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("aba".toCharArray())));
+//        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("abab".toCharArray())));
+//        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("ababa".toCharArray())));
+//        System.out.println(Arrays.toString(instance.prefixSuffixSameLength("aabaaac".toCharArray())));
+//        System.out.println(Arrays.toString(instance.getNext("aabaaac".toCharArray())));
 
         System.out.println(instance.strStr("ababa", "a"));
         System.out.println(instance.strStr("ababa", "ab"));
@@ -36,57 +40,99 @@ public class FindSubstring {
     }
 
 
+//    public int strStr(String haystack, String needle) {
+//        if (needle == null || needle.length() == 0) {
+//            return 0;
+//        }
+//        if (haystack == null || haystack.length() == 0) {
+//            return -1;
+//        }
+//
+//        char[] haystackChars = haystack.toCharArray();
+//        char[] needleChars = needle.toCharArray();
+//        int[] sameHeadTailLength = this.prefixSuffixSameLength(needleChars);
+//
+//        int needleIndex = 0, haystackIndex = 0;
+//        while (haystackIndex < haystackChars.length && needleIndex < needleChars.length) {
+//            if (haystackChars[haystackIndex] == needleChars[needleIndex]) {
+//                haystackIndex++;
+//                needleIndex++;
+//            } else if (needleIndex == 0) {
+//                haystackIndex++;
+//            } else {
+//                needleIndex = sameHeadTailLength[needleIndex - 1]; // 前边已匹配子串的最大前缀后缀长度
+//            }
+//        }
+//        if (needleIndex == needleChars.length) {
+//            return haystackIndex - needleIndex;
+//        } else {
+//            return -1;
+//        }
+//    }
+//
+//    public int[] prefixSuffixSameLength(char[] needleChars) {
+//        int[] prefixSuffixSameLength = new int[needleChars.length];
+//        for (int i = 1; i < needleChars.length; i++) {
+//            char needleChar = needleChars[i];
+//            int prevCharSameLength = prefixSuffixSameLength[i - 1];
+//            if (needleChar == needleChars[prevCharSameLength]) {
+//                prefixSuffixSameLength[i] = prevCharSameLength + 1;
+//            } else {
+//                boolean prefixCharAllSame = true; // fix: "aabaaa" => [0,1,0,1,2,2]
+//                for (int j = 0; j < prevCharSameLength; j++) {
+//                    if (needleChars[j] != needleChar) {
+//                        prefixCharAllSame = false;
+//                        break;
+//                    }
+//                }
+//                if (prefixCharAllSame) {
+//                    prefixSuffixSameLength[i] = prevCharSameLength;
+//                }
+//            }
+//        }
+//        return prefixSuffixSameLength;
+//    }
+
     public int strStr(String haystack, String needle) {
-        if (needle == null || needle.length() == 0) {
+        if (Objects.isNull(needle) || needle.isEmpty()) {
             return 0;
         }
-        if (haystack == null || haystack.length() == 0) {
+        if (Objects.isNull(haystack)|| haystack.isEmpty()) {
             return -1;
         }
-
-        char[] haystackChars = haystack.toCharArray();
-        char[] needleChars = needle.toCharArray();
-        int[] sameHeadTailLength = this.prefixSuffixSameLength(needleChars);
-
-        int needleIndex = 0, haystackIndex = 0;
-        while (haystackIndex < haystackChars.length && needleIndex < needleChars.length) {
-            if (haystackChars[haystackIndex] == needleChars[needleIndex]) {
-                haystackIndex++;
-                needleIndex++;
-            } else if (needleIndex == 0) {
-                haystackIndex++;
+        int[] next = getNext(needle);
+        int i = 0, j = 0;
+        while (i < haystack.length() && j < needle.length()) {
+            if (j == -1 || haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
             } else {
-                needleIndex = sameHeadTailLength[needleIndex - 1]; // 前边已匹配子串的最大前缀后缀长度
+                j = next[j];
             }
         }
-        if (needleIndex == needleChars.length) {
-            return haystackIndex - needleIndex;
+        if (j == needle.length()) {
+            return i - j;
         } else {
             return -1;
         }
     }
 
-    public int[] prefixSuffixSameLength(char[] needleChars) {
-        int[] prefixSuffixSameLength = new int[needleChars.length];
-        for (int i = 1; i < needleChars.length; i++) {
-            char needleChar = needleChars[i];
-            int prevCharSameLength = prefixSuffixSameLength[i - 1];
-            if (needleChar == needleChars[prevCharSameLength]) {
-                prefixSuffixSameLength[i] = prevCharSameLength + 1;
+        public int[] getNext(String needle) {
+        int[] next = new int[needle.length()];
+        next[0] = -1;
+        int k = -1, j = 0;
+        while (j < needle.length() - 1) { // 只用到length-2处即可计算出最后一位(length-1)的next值
+            if (k == -1 || needle.charAt(k) == needle.charAt(j)) {
+                j++;
+                k++;
+                // 查找时needleChars[j]处发生失配后，模式串要回滚到next[j]处做匹配
+                // 此处所隐含的意思是在模式串中 [0...k-1] <==> [j-k...j-1] 相同
+                next[j] = k;
             } else {
-                boolean prefixCharAllSame = true; // fix: "aabaaa" => [0,1,0,1,2,2]
-                for (int j = 0; j < prevCharSameLength; j++) {
-                    if (needleChars[j] != needleChar) {
-                        prefixCharAllSame = false;
-                        break;
-                    }
-                }
-                if (prefixCharAllSame) {
-                    prefixSuffixSameLength[i] = prevCharSameLength;
-                }
+                k = next[k];
             }
         }
-        return prefixSuffixSameLength;
+        return next;
     }
 
 }
