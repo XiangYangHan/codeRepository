3c1a938dd9c58825e26c791be0a75a7c9bd302cf
hanxiangyang <hans.young@outlook.com>
2021-03-03 23:57:01 +0800

    add generate binary tree method generateTreeNode.

--- leetcode/src/main/java/lc/book/binaryTree/TreeNode.java
+++ leetcode/src/main/java/lc/book/binaryTree/TreeNode.java
@@ -1,14 +1,54 @@
 package lc.book.binaryTree;
 
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.Queue;
+
 public class TreeNode {
-    int val;
+    Integer val;
     TreeNode left;
     TreeNode right;
     TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    TreeNode(Integer val) { this.val = val; }
+    TreeNode(Integer val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    public static TreeNode generateTreeNode(Integer[] data) {
+        if (data == null || data.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(data[0]);
+        int left = 1,right = 2;
+        Queue<TreeNode> nodes = new LinkedList<>();
+        nodes.offer(root);
+        while (left < data.length) {
+            TreeNode node = nodes.poll();
+            if (Objects.nonNull(data[left])) {
+                node.left = new TreeNode(data[left]);
+                nodes.offer(node.left);
+            }
+            if (right < data.length && Objects.nonNull(data[right])) {
+                node.right = new TreeNode(data[right]);
+                nodes.offer(node.right);
+            }
+            left += 2;
+            right += 2;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = null;
+        root = generateTreeNode(new Integer[]{});
+        root = generateTreeNode(new Integer[]{1});
+        root = generateTreeNode(new Integer[]{1, null});
+        root = generateTreeNode(new Integer[]{1, null, 2});
+        root = generateTreeNode(new Integer[]{1, null, 2, null});
+        root = generateTreeNode(new Integer[]{1, null, 2, null, 3});
+        root = generateTreeNode(new Integer[]{1, null, 2, null, 3, null});
+        root = generateTreeNode(new Integer[]{1,null,2,3,4,5,null,6,7,8,9,10,11,12,13,null,null, 14});
     }
 }
